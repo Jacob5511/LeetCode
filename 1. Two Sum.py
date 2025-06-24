d6956beae3b9404fc0bacc5a0a68b9413ac82154
Iakov <90548456+Jacob5511@users.noreply.github.com>
2025-06-24 11:25:03 +1000

    1. Two Sum.py

--- 1. Two Sum.py
+++ 1. Two Sum.py
@@ -1,21 +1,10 @@
-class Solution {
-    public int[] twoSum(int[] nums, int target) {
-        HashSet<Integer> s = new HashSet<>();
-        for (int i = 0; i < nums.length; i++) {
-            s.add(nums[i]);
-        }
-        int a, t;
-        for (int i = 0; i < nums.length; i++) {
-            a = nums[i];
-            t = target - a;
-            if (s.contains(t)) {
-                for (int j = i + 1; j < nums.length; j++) {
-                    if (nums[j] == t) {
-                        return new int[]{i, j};
-                    }
-                }
-            }
-        }
-        return nums;
-    }
-}
+class Solution:
+    def twoSum(self, nums: list, target: int):
+        s = set(nums)
+        for i in range(len(nums)):
+            a = nums[i]
+            t = target - a
+            if t in s:
+                for j in range(i+1,len(nums)):
+                    if nums[j] == t:
+                        return [i, j]
